f69ae7bc2eb9fd2d996219123a55a9ddd4031122
Jason Fry <jmphry@gmail.com>
2016-09-15 12:08:34 -0400

    Build board array on game start
    When the player clicks start, boardArray gets built, and when the player
    clicks reset, boardArray is reset to all zeros.

--- app.js
+++ app.js
@@ -2,18 +2,13 @@ $(function() {
 
   var App = {
 
-    q: 1,            // 0 = no game, 1 = player1, -1 = player2
+    q: 0,            // 0 = no game, 1 = player1, -1 = player2
     numOfRows: 6,    // number of rows in the game board
     numOfColumns: 7, // number of columns in the game board
     numToWin: 4,     // number of pieces needed to connect to win
     maxOpacity: 1,   // maximum opacity of game board and start button
     minOpacity: 0.4, // minimum opacity of game board and start button
-    boardArray: [ [0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0] ],
+    boardArray: [],
 
     // determines whose turn it is by changing q
     changeTurn: function() {
@@ -67,6 +62,26 @@ $(function() {
       App.boardArray[App.numOfRows - arrRow][arrColumn] = App.q;
     }, // end modifyArray
 
+    // resets game by setting data-row to 0 for all columns in the game board,
+    // setting App.q to 0, dimming the board, and resetting App.boardArray
+    resetGame: function() {
+      for (var i = 0; i < App.numOfColumns; i++) {
+        columnId = 'column' + i.toString();
+        $('#'+columnId).attr('data-row', '0');
+      } // end for
+      App.q = 0;
+      UI.changeOpacity($('#board'));
+      App.boardArray(App.numOfColumns).fill(0);
+      for (var i = 0; i < App.numOfColumns; i++) {
+        App.boardArray[i](App.numOfRows).fill(0);
+      } // end for
+      App.createBoardArray();
+    }, // end resetGame
+
+    createBoardArray: function() {
+      App.boardArray = Array(App.numOfRows).fill(Array(App.numOfColumns).fill(0));
+    },
+
     // // starts the game by setting var q to 1.
     // startGame: function() {
     //   App.q = 1;
@@ -107,9 +122,6 @@ $(function() {
   var UI = {
       // modifies the argument's opacity from 0.5 to 1 or from 1 to 0.5
     changeOpacity: function(boardOrButton) {
-
-      // var boardOrButton = this.boardOrButton;
-      console.log('boardOrButton: ', boardOrButton.css('opacity'));
       if ( boardOrButton.css('opacity') < App.maxOpacity ) {
         boardOrButton.fadeTo( 800, App.maxOpacity );
       } // end if
@@ -129,7 +141,7 @@ $(function() {
     }, // end moveGamePiece
 
     confirmResetGame: function() {
-
+      App.resetGame();
     }, // end confirmResetGame
 
   }; // end UI
@@ -142,16 +154,18 @@ $(function() {
         App.changeTurn();
         Display.displayTurn();
         UI.changeOpacity($('#board'));
+        App.createBoardArray();
       }
     }, // end onClickStart
 
     onClickReset: function() {
-      App.q = 0;
+      UI.confirmResetGame();
     }, // end onClickReset
 
     onClickColumn: function() {
       App.incrementDataRow($(this));
       App.modifyArray($(this));
+      App.checkForWinner();
     }, // end onClickColumn
 
     // Optional
@@ -161,6 +175,7 @@ $(function() {
 
   }; // end EventHandlers
   $('#start').click(EventHandlers.onClickStart);
+  $('#reset').click(EventHandlers.onClickReset);
   $('#column6').click(EventHandlers.onClickColumn);
   $('#column5').click(EventHandlers.onClickColumn);
   $('#column4').click(EventHandlers.onClickColumn);
